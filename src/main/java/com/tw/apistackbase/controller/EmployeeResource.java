8039fd81ee176f292bf92d753fa0fc32b47c5234
jacky.leung <jacky.leung@oocl.com>
2018-11-27 18:07:56 +0800

    [LEUNGJA] feat: add employee

--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -2,20 +2,30 @@ package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.employee.Boss;
 import com.tw.apistackbase.employee.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeResource {
+    private Boss boss;
+
+    @Autowired
+    public EmployeeResource(Boss boss) {
+        this.boss = boss;
+    }
 
     @GetMapping(path = "/AllEmployees", produces = {"application/json"})
     public @ResponseBody List<Employee> getAll() {
-        Boss boss = new Boss();
         return boss.getEmployeeList();
+    }
+
+    @PostMapping(path = "/Employee")
+    public Employee add(@RequestBody Employee newEmployee) {
+        newEmployee.id = boss.getNextID();
+        boss.addEmployee(newEmployee);
+        return newEmployee;
     }
 }
--- src/main/java/com/tw/apistackbase/employee/Boss.java
+++ src/main/java/com/tw/apistackbase/employee/Boss.java
@@ -1,22 +1,39 @@
 package com.tw.apistackbase.employee;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class Boss {
     private List<Employee> employeeList;
+    private int nextID;
 
     public Boss() {
         employeeList = new ArrayList<>();
-        this.addEmployee(new Employee(0, "Xiaoming", 20, "Male"));
-        this.addEmployee(new Employee(1, "Xiaohong", 19, "Female"));
+        nextID = 0;
     }
 
     public void addEmployee(Employee employee) {
         employeeList.add(employee);
+        nextID++;
+    }
+
+    public boolean removeEmployee(Employee employee) {
+        if (employeeList.contains(employee)) {
+            return false;
+        }
+        employeeList.remove(employee);
+        return true;
+
     }
 
     public List<Employee> getEmployeeList() {
         return employeeList;
+    }
+
+    public int getNextID() {
+        return nextID;
     }
 }
