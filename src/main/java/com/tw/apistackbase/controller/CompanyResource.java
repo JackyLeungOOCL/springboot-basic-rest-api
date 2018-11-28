fe35b2d40a504a38ad8c79cc49848b1980ecabbe
jacky.leung <jacky.leung@oocl.com>
2018-11-28 15:41:05 +0800

    [LEUNGJA] update: society add company with 201, location

--- src/main/java/com/tw/apistackbase/controller/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyResource.java
@@ -4,11 +4,18 @@ import com.tw.apistackbase.Company;
 import com.tw.apistackbase.Employee;
 import com.tw.apistackbase.Society;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+
+import static java.net.URI.*;
 
 @RestController
 @RequestMapping("/companies")
@@ -47,11 +54,22 @@ public class CompanyResource {
         }
     }
 
+//    @PostMapping()
+//    public Company add(@RequestBody Company newCompany) {
+//        newCompany.id = society.getNextID();
+//        society.addCompany(newCompany);
+//        return newCompany;
+//    }
+
     @PostMapping()
-    public Company add(@RequestBody Company newCompany) {
+    public ResponseEntity<Company> add(@RequestBody Company newCompany, UriComponentsBuilder b) {
         newCompany.id = society.getNextID();
         society.addCompany(newCompany);
-        return newCompany;
+
+        UriComponents uriComponents = b.path("/companies/{id}").buildAndExpand(newCompany.id);
+        return ResponseEntity
+                .created(uriComponents.toUri())
+                .body(newCompany);
     }
 
     @PutMapping(path = "/{id}")
